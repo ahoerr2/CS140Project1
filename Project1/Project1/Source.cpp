cdafefdc8818a096524600cd8478aebc6feac1fa
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-10-04 17:27:53 -0500

    Setup encryption

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,9 +2,9 @@
 
 /*TODO: (* finished) ($ finished needs review) (- work in progress)
 *
-		Accept Input(Key) from user $
-		Check if input is valid(Make uppercase) -
-		Store the substitution key
+		Accept Input(Key) from user *
+		Check if input is valid(Make uppercase) *
+		Store the substitution key *
 		Accept a plaintext message from user
 		Encrypt the message
 		Display message to user
@@ -24,12 +24,19 @@ using std::endl;
 //Skip user input and use a randomly generated key(debug)
 bool SKIP_USER_INPUT = false;
 
+//Debug String
+string DEBUG_STRING = "All poets, all writers are political. They either maintain the status quo, or they say, ’Something’s wrong, let’s change it for the better.";
+
+
 //Debug mode
 bool DEBUG_MODE = true;
 
 bool validateUserKeyInput(string);
 string grabUserInputKey();
 string generateRandomKey();
+string toUpperCase(string);
+string monoSubEncrypt(string message, string key);
+
 
 int main()
 {
@@ -37,26 +44,35 @@ int main()
 	cout << "Hello, enter a monoalphabetic key for encryption (26 letters long)." << endl;
 	cout << "Leave the key black to use a randomly generated key." << endl;
 	string userInputKey = grabUserInputKey();
-	for (char& c : userInputKey) c = toupper(c);
+	userInputKey = toUpperCase(userInputKey);
+
 
 	if (userInputKey.length() == 0) userInputKey = generateRandomKey();
 
-		if (DEBUG_MODE)
-		{
-			cout << userInputKey << endl;
-			cout << "Size: " << userInputKey.length() << endl;
-		}
+	if (DEBUG_MODE)
+	{
+		cout << userInputKey << endl;
+		cout << "Size: " << userInputKey.length() << endl;
+	}
 
 	//Promts user to enter a new key is the key was invalid
 	bool keyInputSuccessful = validateUserKeyInput(userInputKey);
+
 	while (keyInputSuccessful != true)
 	{
 		cout << "The key is invalid, please input another key." << endl;
 		userInputKey = grabUserInputKey();
 		keyInputSuccessful = validateUserKeyInput(userInputKey);
 	}
+
 	cout << "Key input successful" << endl;
 	cout << "Key: " << userInputKey << "\n" << endl;
+
+
+}
+
+string toUpperCase(string inputString) {
+	for (char& c : inputString) c = toupper(c);
 }
 
 //generates an uppercase MASC key by shuffling the English alphabet
@@ -89,4 +105,9 @@ bool validateUserKeyInput(string key)
 	}
 
 	return true;
+}
+
+
+string monoSubEncrypt(string message, string key) {
+
 }
