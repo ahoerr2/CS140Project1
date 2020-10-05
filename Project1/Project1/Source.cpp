c477be939ae9aaae68493052dbfa3ff358090f13
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-10-05 13:57:24 -0500

    Finished Encryption

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -29,15 +29,17 @@ string DEBUG_STRING = "The fitness gram pacer test, is a multi stage test, that.
 //string DEBUG_STRING = "ABCD";
 
 //Debug mode
-bool DEBUG_MODE = true;
-bool DEBUG_ENCRYPT = true;
+bool DEBUG_MODE = false;
+bool DEBUG_ENCRYPT = false;
 
 bool validateUserKeyInput(string);
 string grabUserInputKey();
 string generateRandomKey();
 string toUpperCase(string);
-string monoSubEncrypt(string& message, string key);
+string monoSubEncrypt(string, string, int);
 int grabUserSpacing();
+bool isValidChar(char);
+void validatePlaintext(string&, int spacing);
 
 
 int main()
@@ -72,8 +74,9 @@ int main()
 
 	//Prompts user to enter plaintext for translation
     	cout << "Enter the plaintext: " << endl;
-   	 string plaintext = " ";
+   	 string plaintext = "";
    	 std::getline(cin , plaintext);
+	 
     
   	  if(DEBUG_MODE)
    	 {
@@ -84,14 +87,15 @@ int main()
    	 cout << "How many letters should appear in each grouping? " << endl << "(Press enter for a default spacing of 5, negative entry provides original spacing.)" << endl;
     
    	 int userSpacing = grabUserSpacing();
-    
+	 validatePlaintext(plaintext,userSpacing);
    	 if(DEBUG_MODE)
      	   cout << "Spacing input: " << userSpacing << endl;
 	
-	if (DEBUG_ENCRYPT) {
-		cout << "Message: " << DEBUG_STRING << endl;
-		cout << "Encrypted Message: " << monoSubEncrypt(DEBUG_STRING, userInputKey);
-	}
+	 if (DEBUG_MODE) {
+		 cout << "Message: " << plaintext << endl;
+	 }
+		cout << "Encrypted Message: " << monoSubEncrypt(plaintext, userInputKey, userSpacing);
+	
 }
 
 string toUpperCase(string inputString) {
@@ -137,43 +141,73 @@ int grabUserSpacing()
     int inputSpace;
     
     cout << "Spacing: ";
+	if (cin.peek() == '\n') return spacing;
     cin >> inputSpace;
-    
-    if(inputSpace >= 1)
-    {
+
+	if (inputSpace == 0) return spacing;
         spacing = inputSpace;
-    }
+
     
     return spacing;
 }
 
-string monoSubEncrypt(string& message, string key) {
+bool isValidChar(char c) {
+	if ((c > 96 && c < 123) || (c > 64 && c < 91)) return true;
+	return false;
+}
+
+void validatePlaintext(string& plaintext, int spacing) {
+	for (int index = 0; index < plaintext.length(); index++) {
+		if (isValidChar(plaintext.at(index))) continue;
+		else if (plaintext.at(index) == ' ' && spacing <= -1) continue;
+		else { 
+			plaintext.erase(plaintext.begin() + index); 
+			index--;
+		
+		}
+	}
+}
+
+
+string monoSubEncrypt(string message, string key, int spacing) {
 
 	int index = 0;
 	char c;
 	string encryptedMessage = "";
+	int spacingIndex = 0;
+	int encryptedLength = message.length() + (message.length() / spacing);
 
 	for (unsigned index{ 0 }; index < message.length(); index++) {
 		//Checks if character is valid and uppercase/lowercase, otherwise skip encryption
 		c = message.at(index);
+
+		if (spacing <= -1 && c == ' ') {
+			encryptedMessage += ' ';
+			continue;
+		}
+
 		if (c > 96 && c < 123) {
 			c -= 97;
 		}
 		else if (c > 64 && c < 91) {
 			c -= 65;
 		}
-		else { 
-			message[index] = c;
-			continue; 
-		}
 
 		if (DEBUG_MODE) {
 			cout << "c: "<< message.at(index) << " = "<< static_cast<int>(c) << endl;
 		}
 
-		message[index] = key.at(static_cast<int>(c));
+		if ((spacingIndex % spacing == 0 && index != 0) && spacing >= 1) {
+			encryptedMessage += ' ';
+			spacingIndex = 0;
+		}
+
+		encryptedMessage += key.at(static_cast<int>(c));
+		spacingIndex++;
 
 	}
 
-	return message;
+	return encryptedMessage;
 }
+
+
